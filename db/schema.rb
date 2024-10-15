c76b21b49c7a0d611de89458c9d12bfefa5cf120
Patryk <ppatryk8@op.pl>
2024-10-15 03:32:01 +0200

    Add page-view field to BlogPost model, implement validations and add tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_15_003021) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_15_010849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_15_003021) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe BlogPost, type: :model do
       title: "Hello!",
       content: "Hello" * 20,
       created_at: Time.current,
-      updated_at: Time.current
+      updated_at: Time.current,
+      views: 10,
     )
     expect(post).to be_valid
   end
@@ -15,6 +16,7 @@ RSpec.describe BlogPost, type: :model do
     post = BlogPost.create!(
       title: "BlogPost Title",
       content: "Hello! its valid content" * 20,
+      views: 10,
     )
      expect(post).to be_valid 
      post.title = "BlogPost Title" * 500
@@ -25,17 +27,35 @@ RSpec.describe BlogPost, type: :model do
      expect(post).to_not be_valid 
   end
 
-  it "has a valid body" do 
+  it "has a valid content" do 
     post = BlogPost.create!(
       title: "BlogPost Title",
       content: "Hello! its valid content" * 20,
+      views: 10,
     )
      expect(post).to be_valid 
      post.content = "BlogPost content" * 1500
      expect(post).to_not be_valid
-     post.content = "a" 
+     post.content = " "
      expect(post).to_not be_valid 
      post.content = ""
      expect(post).to_not be_valid 
+  end
+
+  it "views is valid" do 
+    numeric = BlogPost.create!(
+      title: "BlogPost Tilte",
+      content: "Hello! its valid content!" * 20,
+      views: 10,
+    )
+    expect(numeric).to be_valid 
+    numeric.views = 0
+    expect(numeric).to_not be_valid
+    numeric.views = 55000
+    expect(numeric).to_not be_valid
+    numeric.views = 99999
+    expect(numeric).to_not be_valid
+    numeric.views = -1
+    expect(numeric).to_not be_valid
   end
 end
