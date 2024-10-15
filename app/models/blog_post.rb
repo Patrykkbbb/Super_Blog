c694e4e2c430537a07c9167b54d58b7c9f45080e
Patryk <ppatryk8@op.pl>
2024-10-15 02:31:59 +0200

    Add and implement ActionText

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,6 @@
 class BlogPost < ApplicationRecord
+    has_rich_text :content
+
     validates :title, presence: true, length: { in: 5..70 }
     validates :content, presence: true, length: { in: 20..1500 }
 end
