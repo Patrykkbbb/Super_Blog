af695e2bf64bc3efaf501375488a613fbed8937e
Patryk <ppatryk8@op.pl>
2024-10-16 01:42:53 +0200

    Add field test for admin; add new fields to admin model

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "admin fields be valid" do
+    admin = Admin.create!(
+    email: "admin@example.com",
+    password: "password",
+    password_confirmation: "password"
+    ) 
+    expect(admin).to be_valid 
+    admin.nickname = "admin"
+    expect(admin).to be_valid
+    admin.first_name = "admin"
+    expect(admin).to be_valid
+    admin.last_name = "admin"
+    expect(admin).to be_valid
+  end
 end
