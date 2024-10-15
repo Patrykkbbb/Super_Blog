67884c52357ad4e5d80cca42bdc288b9c20f4d59
Patryk <ppatryk8@op.pl>
2024-10-16 01:24:08 +0200

    Add validations to nickname in user model; test nickname validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { in: 4..40 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  it "user exist" do 
+  it "nickname should be valid" do 
   user = User.create!(
     email: "patryk2@example.com",
     password: "password",
     password_confirmation: "password",
-    nickname = "username",
-    first_name = "user",
-    last_name = "name"
+    nickname: "username",
+    first_name: "user",
+    last_name: "name"
   )
   expect(user).to be_valid
+  user.nickname = ""
+  expect(user).to_not be_valid 
+  user.nickname = "a" * 500
+  expect(user).to_not be_valid
+  user.nickname = "a"
+  expect(user).to_not be_valid
   end
 end
