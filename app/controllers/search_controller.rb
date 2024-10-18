52e42212e280bced9b0bfc6853305bf6ef3ed475
Patryk <ppatryk8@op.pl>
2024-10-18 02:15:54 +0200

    Add search results page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController 
+    before_action :authenticate_user!
+    
     def index
       @query = BlogPost.ransack(params[:q])
       @blog_post = @query.result(distinct: true)
