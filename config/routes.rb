d11ca73ff397b351885e96aa9c6160d33b7cbc26
Patryk <ppatryk8@op.pl>
2024-10-20 02:51:19 +0200

    Implement user removal functionality for admins; add Polish translations for user index and show

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     get "user/:id", to: "user#show", as: :user
     get "users/", to: "user#index", as: :user_list
+    delete "user/:id", to: "user#destroy"
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
     # Can be used by load balancers and uptime monitors to verify that the app is live.
     get "up" => "rails/health#show", as: :rails_health_check
