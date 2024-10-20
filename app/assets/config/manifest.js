c8e11c172acf1d0054183687d09567c3791d3ca7
Patryk <ppatryk8@op.pl>
2024-10-20 20:52:53 +0200

    Setup bootstrap js in config

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,9 +2,8 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+import 'bootstrap'
 
 //= require jquery3
 //= require popper
 //= require bootstrap
-
-import "bootstrap";
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'bootstrap'
 
 import "trix"
 import "@rails/actiontext"
-import "bootstrap";
 
 //= require jquery3
 //= require popper
-//= require bootstrap
+//= require bootstrap-sprockets
